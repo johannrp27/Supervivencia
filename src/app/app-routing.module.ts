b510f85ced010e4baaa27332d9e43aedb72b5fc8
Johann <johannrip@gmail.com>
2020-08-22 17:06:20 -0500

    New Design + Segmented Pages + Restructured

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FaqComponent } from './views/faq/faq.component';
+import { HomeComponent } from './views/home/home.component';
+import { AboutComponent } from './views/about/about.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'faq',
+    component: FaqComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,18 @@ import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NgxImageZoomModule } from 'ngx-image-zoom';
 import { NgxGalleryModule } from 'ngx-gallery-9';
+import { AboutComponent } from './views/about/about.component';
+import { FaqComponent } from './views/faq/faq.component';
+import { HomeComponent } from './views/home/home.component';
+import { CatalogComponent } from './views/catalog/catalog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    FaqComponent,
+    HomeComponent,
+    CatalogComponent
   ],
   imports: [
     BrowserModule,
